b9ec8491240c92f196fece6a3c117fd11635e88b
Dazhy Khovalyg <hovalyg_d@mail.ru>
2024-12-19 21:36:57 +0300

    l1-7 done

--- l1.7/l1-7.go
+++ l1.7/l1-7.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
+	safeMap := new(sync.Map)
+	wg := new(sync.WaitGroup)
 
+	// Конкурентная запись в map
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			safeMap.Store(i, i*i)
+			fmt.Printf("wrote %v at %v\n", i*i, i)
+		}(i)
+	}
+
+	wg.Wait()
+
+	// Чтение из map
+	safeMap.Range(func(key, value interface{}) bool {
+		fmt.Printf("got %v at %v\n", value, key)
+		return true
+	})
 }
