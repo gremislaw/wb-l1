29e106a0f65dbeb0a6addf5a5db20230b67c611a
Dazhy Khovalyg <hovalyg_d@mail.ru>
2024-12-19 18:29:01 +0300

    l1-1 done

---
+++ l1.1/l1-1.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type Human struct {
+	name string
+	age int
+}
+
+func (h Human) SayHello() {
+	fmt.Printf("Hello! My name is %v and i'm %v years old\n", h.name, h.age)
+}
+
+type Action struct {
+	Human
+}
+
+func main() {
+	human := Human{
+		name: "Dazhy",
+		age: 20,
+	}
+	
+	action := Action{
+		Human: human,
+	}
+
+	action.SayHello()
+}
