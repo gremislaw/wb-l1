f8f6af85f418b6419f24467b533642d6d36b5336
Dazhy Khovalyg <hovalyg_d@mail.ru>
2024-12-19 20:20:05 +0300

    l1-5 done

--- l1.5/l1-5.go
+++ l1.5/l1-5.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
+	var duration int
+	fmt.Print("Timer: ")
+	fmt.Scan(&duration)
 
+	// Таймер на завершение программы
+	timer := time.NewTimer(time.Duration(duration) * time.Second)
+
+	ch := make(chan int)
+
+	// Горутина для отправки данных в канал
+	go func() {
+		for {
+			ch <- rand.Int()
+		}
+	}()
+
+	// Горутина для чтения данных из канала
+	go func() {
+		for {
+			fmt.Printf("Got: %d\n", <-ch)
+		}
+	}()
+
+	<-timer.C
+	fmt.Println("Timeout. Shutting down...")
 }
