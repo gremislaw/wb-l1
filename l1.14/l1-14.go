ac99a1e9977052427119d2d5d83e8a312460b545
Dazhy Khovalyg <hovalyg_d@mail.ru>
2024-12-21 23:09:06 +0300

    l1-14 done

--- l1.14/l1-14.go
+++ l1.14/l1-14.go
@@ -1,7 +1,38 @@
+// Разработать программу, которая в рантайме способна определить тип
+// переменной: int, string, bool, channel из переменной типа interface{}.
+
 package main
 
 import "fmt"
 
-func main() {
+func printType(v interface{}) {
+	switch v.(type) {
+	case int:
+		fmt.Println("int")
+	case string:
+		fmt.Println("string")
+	case bool:
+		fmt.Println("bool")
+	case chan any:
+		fmt.Println("channel")
+	default:
+		fmt.Println("unknown type")
+	}
+}
 
+func main() {
+	var intValue interface{} = 17
+	printType(intValue)
+
+	var stringValue interface{} = "17"
+	printType(stringValue)
+
+	var boolValue interface{} = true
+	printType(boolValue)
+
+	var chanValue1 interface{} = make(chan any)
+	printType(chanValue1)
+
+	var chanValue3 interface{} = 1.7
+	printType(chanValue3)
 }
