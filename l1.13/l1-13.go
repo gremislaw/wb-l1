9945ea6169a7eed518260994428607df5a569b3e
Dazhy Khovalyg <hovalyg_d@mail.ru>
2024-12-21 22:50:09 +0300

    l1-13 done

--- l1.13/l1-13.go
+++ l1.13/l1-13.go
@@ -1,7 +1,17 @@
+// Поменять местами два числа без создания временной переменной.
+
 package main
 
 import "fmt"
 
 func main() {
+	a := 4
+	b := 11
 
+	fmt.Println(a, b)
+
+	// Множественное присваивание
+	a, b = b, a
+
+	fmt.Println(a, b)
 }
