06b524efbf88d9e1cadc03866bb5359b37227707
Dazhy Khovalyg <hovalyg_d@mail.ru>
2024-12-23 18:43:39 +0300

    l1-20 done

--- l1.20/l1-20.go
+++ l1.20/l1-20.go
@@ -1,7 +1,28 @@
+// Разработать программу, которая переворачивает слова в строке.
+// Пример: «snow dog sun — sun dog snow».
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func reverseWords(s string) string {
+	resWords := make([]string, 0)
+
+	// Разбивка строки на слайс из слов
+	words := strings.Split(s, " ")
+	for i := len(words) - 1; i >= 0; i-- {
+		resWords = append(resWords, words[i])
+	}
+
+	// Объединение слайса слов в строку
+	res := strings.Join(resWords, " ")
+	return res
+}
 
 func main() {
-
+	example := "snow dog sun"
+	fmt.Println(reverseWords(example))
 }
