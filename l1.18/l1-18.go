aa29cff12c7ec908071a26170386b30facb6f32b
Dazhy Khovalyg <hovalyg_d@mail.ru>
2024-12-23 18:18:32 +0300

    l1-18 done

--- l1.18/l1-18.go
+++ l1.18/l1-18.go
@@ -1,7 +1,43 @@
+// Реализовать структуру-счетчик, которая будет инкрементироваться в конкурентной среде.
+// По завершению программа должна выводить итоговое значение счетчика.
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+type Counter struct {
+	mu    sync.Mutex
+	count int
+}
+
+func (c *Counter) Increment() {
+	c.mu.Lock()
+	c.count++
+	c.mu.Unlock()
+}
+
+func (c *Counter) GetCount() int {
+	return c.count
+}
 
 func main() {
+	counter := new(Counter)
 
+	fmt.Println(counter.GetCount())
+
+	wg := new(sync.WaitGroup)
+	n := 100000
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			counter.Increment()
+		}()
+	}
+	wg.Wait()
+
+	fmt.Println(counter.GetCount())
 }
