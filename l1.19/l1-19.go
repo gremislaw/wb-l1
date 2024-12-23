71823cd5d951cfa96cd8a1fa1c0d67cd3da197a3
Dazhy Khovalyg <hovalyg_d@mail.ru>
2024-12-23 18:34:30 +0300

    l1-19 done

--- l1.19/l1-19.go
+++ l1.19/l1-19.go
@@ -1,7 +1,23 @@
+// Разработать программу, которая переворачивает подаваемую на ход строку
+// (например: «главрыба — абырвалг»). Символы могут быть unicode.
+
 package main
 
 import "fmt"
 
-func main() {
+func reverse(s string) string {
+	res := make([]rune, 0)
 
+	// Преобразование строки в срез рун для поддержки Unicode
+	sRune := []rune(s)
+	for i := len(sRune) - 1; i >= 0; i-- {
+		res = append(res, sRune[i])
+	}
+
+	return string(res)
+}
+
+func main() {
+	example := "главрыба"
+	fmt.Println(reverse(example))
 }
