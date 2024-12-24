5d08b7e171833e8cd83b924661ac056f243ce9a7
Dazhy Khovalyg <hovalyg_d@mail.ru>
2024-12-24 13:15:21 +0300

    l1-21 done

--- l1.21/l1-21.go
+++ l1.21/l1-21.go
@@ -1,7 +1,41 @@
+// Реализовать паттерн «адаптер» на любом примере.
+
 package main
 
 import "fmt"
 
-func main() {
+// Target представляет интерфейс, с которым будет работать система
+type Target interface {
+	Request() string
+}
 
+// Adaptee - несовместивая структура к нашей системе
+type Adaptee struct {
+}
+
+// NewAdapter - это конструктор Adapter
+func NewAdapter(adaptee *Adaptee) Target {
+	return &Adapter{adaptee}
+}
+
+// Несоответствующий интерфейсу функционал
+func (a *Adaptee) SpecificRequest() string {
+	return "Request"
+}
+
+// Adapter реализует интерфейс target
+type Adapter struct {
+	*Adaptee
+}
+
+// Адаптирование несовместимого функционала
+func (a *Adapter) Request() string {
+	return a.SpecificRequest()
+}
+
+func main() {
+	adaptee := &Adaptee{}
+	target := NewAdapter(adaptee)
+
+	fmt.Println(target.Request())
 }
