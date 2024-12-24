db27794eece72abd7bc703794f4671de38dad7e4
Dazhy Khovalyg <hovalyg_d@mail.ru>
2024-12-24 13:35:18 +0300

    l1-22 done

--- l1.22/l1-22.go
+++ l1.22/l1-22.go
@@ -1,7 +1,22 @@
+// Разработать программу, которая перемножает, делит, складывает, вычитает две
+// числовых переменных a и b, значение которых > 2^20.
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/big"
+)
 
 func main() {
+	a, _ := big.NewFloat(0).SetString("9223372036854775808")   // число больше int64
+	b, _ := big.NewFloat(0).SetString("100000000000000000001") // число больше 10 ^ 20
 
+	var tmp big.Float
+	fmt.Println("a + b = ", tmp.Add(a, b))
+	fmt.Println("a - b = ", tmp.Add(a, tmp.Neg(b)))
+	fmt.Println("b - a = ", tmp.Add(tmp.Neg(a), b))
+	fmt.Println("a * b = ", tmp.Mul(a, b))
+	fmt.Println("a / b = ", tmp.Quo(a, b))
+	fmt.Println("b / a = ", tmp.Quo(b, a))
 }
