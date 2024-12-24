398962f3b75855eee4a499df7a3ae5985c7e5214
Dazhy Khovalyg <hovalyg_d@mail.ru>
2024-12-24 21:00:04 +0300

    done l1-26

--- l1.26/l1-26.go
+++ l1.26/l1-26.go
@@ -1,7 +1,32 @@
+// Разработать программу, которая проверяет, что все символы в строке уникальные
+// (true — если уникальные, false etc). Функция проверки должна быть
+// регистронезависимой.
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func IsBeauty(s string) bool {
+	// Приведение к нижнему регистру для регистронезависимой проверки
+	s = strings.ToLower(s)
+
+	// Создадим множество для отслеживания встреченных символов
+	checkMap := make(map[rune]struct{}, 0)
+	for _, sym := range s {
+		if _, ok := checkMap[sym]; !ok {
+			checkMap[sym] = struct{}{}
+		} else {
+			return false
+		}
+	}
+	return true
+}
 
 func main() {
-
+	fmt.Println(IsBeauty("abcd"))      // true
+	fmt.Println(IsBeauty("abCdefAaf")) // false
+	fmt.Println(IsBeauty("aabcd"))     // false
 }
