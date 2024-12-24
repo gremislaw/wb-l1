28b8132c9de56949712133d66e9fc4aacfc53a4e
Dazhy Khovalyg <hovalyg_d@mail.ru>
2024-12-24 13:43:54 +0300

    l1-23 done

--- l1.23/l1-23.go
+++ l1.23/l1-23.go
@@ -1,7 +1,24 @@
+// Удалить i-ый элемент из слайса.
+
 package main
 
 import "fmt"
 
-func main() {
+func removeElement(arr []int, index int) ([]int, error) {
+	if index < 0 || index >= len(arr) {
+		return arr, fmt.Errorf("index out of range") // Вернем тот же массив
+	}
+	return append(arr[:index], arr[index+1:]...), nil
+}
 
+func main() {
+	arr := []int{1, 2, 3, 4, 5}
+	fmt.Println(arr)
+
+	var err error
+	arr, err = removeElement(arr, 3) // Удаление элемента под индексом 3
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(arr)
 }
